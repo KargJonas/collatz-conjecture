583b455733c60911553ee5948c3279435397e20d
Jonas Karg <jonas.karg@hotmail.com>
2021-08-08 21:17:40 +0200

    scale

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const ctx = cnv.getContext('2d');
 cnv.width = innerWidth;
 cnv.height = innerHeight;
 
-for (let i = 1; i < 10000; i++) {
+for (let i = 1; i < 5000; i++) {
   ctx.moveTo(i, i);
   ctx.beginPath();
   ctx.strokeStyle = 'rgba(255, 255, 255, 0.1)';
@@ -16,7 +16,7 @@ for (let i = 1; i < 10000; i++) {
     if (x % 2 === 0) x /= 2;
     else x = (x * 3 + 1);
 
-    ctx.lineTo(n * 4, x / 10000);
+    ctx.lineTo(n * 7, x / 1000);
   }
 
   ctx.stroke();
